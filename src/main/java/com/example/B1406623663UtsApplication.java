1c8adb53cc3158faf2f2ce464331de00c9008285
Reitasya Devina <reitasya.devina@gmail.com>
2017-10-23 12:56:04 +0700

    1st commit, baru package

---
+++ src/main/java/com/example/B1406623663UtsApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B1406623663UtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B1406623663UtsApplication.class, args);
+	}
+}
